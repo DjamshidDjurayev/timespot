7def70fedc09fcc53324d106832420dc8c5baef2
Djamshid <djurayev.d@gmail.com>
2019-09-07 13:30:08 +0500

    mqtt bug fix

--- app/constants.scala
+++ app/constants.scala
@@ -1,11 +1,11 @@
 package object constants {
   val FCMKey: String = "AAAAIr9zZnk:APA91bFuiPycVWFSclIlcxZOmkgTD_QPk1nxtTAnJjj1NbvzMvmSKZXjBT2Tr18NYOncwgyjI1PkeGauivrvTZINqTSPcCaOonx83bplyETRRpophuYNvSyPNkkFM0AtlKFeLh6S3sEn"
-//  val DB_URL: String = "jdbc:postgresql://ec2-174-129-18-170.compute-1.amazonaws.com:5432/dd83d204polrve"
-//  val DB_USER: String = "ntlmnyvtuvpwpv"
-//  val DB_PASSWORD: String = "_AwM27MHmyBzkIXPPDOVu1I32L"
-  val DB_URL: String = "jdbc:h2:mem:play"
-  val DB_USER: String = ""
-  val DB_PASSWORD: String = ""
+  val DB_URL: String = "jdbc:postgresql://ec2-174-129-18-170.compute-1.amazonaws.com:5432/dd83d204polrve"
+  val DB_USER: String = "ntlmnyvtuvpwpv"
+  val DB_PASSWORD: String = "_AwM27MHmyBzkIXPPDOVu1I32L"
+//  val DB_URL: String = "jdbc:h2:mem:play"
+//  val DB_USER: String = ""
+//  val DB_PASSWORD: String = ""
   val BROKER_URL: String = "tcp://soldier.cloudmqtt.com:14969"
 
   val CLOUDINARY_API_SECRET = "gx5pxezG25hPdqvNEEk8GWdeTcQ"
