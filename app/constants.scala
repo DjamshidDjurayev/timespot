73e945078d33d4908045b826f50d3ca28bf2a8e6
Djamshid <djurayev.d@gmail.com>
2020-03-25 11:02:57 +0500

    bugfix

--- app/constants.scala
+++ app/constants.scala
@@ -1,7 +1,7 @@
 package object constants {
   val FCMKey: String = "AAAAIr9zZnk:APA91bFuiPycVWFSclIlcxZOmkgTD_QPk1nxtTAnJjj1NbvzMvmSKZXjBT2Tr18NYOncwgyjI1PkeGauivrvTZINqTSPcCaOonx83bplyETRRpophuYNvSyPNkkFM0AtlKFeLh6S3sEn"
   val FCMUrl: String = "https://fcm.googleapis.com/fcm/send"
-  val DB_URL: String = "postgres://yfbzjiqiiiuczr:1f9ff9a20a078e12b0cf0a5b8749370d18c142f322fabe8906093d589b51a3fd@ec2-35-172-85-250.compute-1.amazonaws.com:5432/d7l21tuaqv7hdg"
+  val DB_URL: String = "jdbc:postgresql://ec2-35-172-85-250.compute-1.amazonaws.com:5432/d7l21tuaqv7hdg"
   val DB_USER: String = "yfbzjiqiiiuczr"
   val DB_PASSWORD: String = "1f9ff9a20a078e12b0cf0a5b8749370d18c142f322fabe8906093d589b51a3fd"
 //  val DB_URL: String = "jdbc:h2:mem:play"
