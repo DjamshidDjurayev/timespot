1f8ffb91c5d1e31b8caeaf8e7f6e31d60d8e56e3
Djamshid <djurayev.d@gmail.com>
2020-05-07 14:33:12 +0500

    bugfix

--- app/models/Db.scala
+++ app/models/Db.scala
@@ -32,6 +32,6 @@ object Db extends Instance(
   url = constants.DB_URL,
   user = constants.DB_USER,
   password = constants.DB_PASSWORD,
-  initMode = InitMode.Create
+  initMode = InitMode.DropCreate
 )
 
